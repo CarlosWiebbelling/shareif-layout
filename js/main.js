da0741ab685ce8a47d71c81ad536d6840b553909
Róger <rogercascaes@hotmail.com>
2019-12-03 20:58:27 -0200

    merge login page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let nome = "Anônimo";
+var nome = recebeNome("nome");
 let chatOpen = undefined
 
 // FUNÇÃO DE LOGIN VIA TECLA ENTER.
@@ -14,9 +14,28 @@ function login_enter(){
 
 // FUNÇÃO DE LOGIN.
 function login(){
-    let novoUsuario = document.getElementById('chave').value;
-    localStorage.setItem('userShareif', novoUsuario)
-    window.location.href = "pgs/chat.html";
+    let chave = document.getElementById('chave').value;
+    window.location.href = `pgs/chat.html?nome=${ chave }`;
+}
+
+// ATRIBUI O NOME AO USUÁRIO.
+function recebeNome(parameter) {  
+    var loc = location.search.substring(1, location.search.length);   
+    var param_value = false;   
+    var params = loc.split("&"); 
+
+    for (i=0; i<params.length;i++) {   
+        param_name = params[i].substring(0,params[i].indexOf('='));   
+        if (param_name == parameter) {                                          
+            param_value = params[i].substring(params[i].indexOf('=')+1)   
+        }   
+    }   
+
+    if (param_value) {   
+        return param_value;   
+    } else {   
+        return undefined;   
+    }   
 }
 
 // FUNÇÃO PARA ENVIAR A MENSAGEM NO CHAT VIA TECLA "ENTER".
